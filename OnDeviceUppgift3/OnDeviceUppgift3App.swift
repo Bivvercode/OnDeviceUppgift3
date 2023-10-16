f61d6b31fc8d7323cf26b937845b0883be5eeeba
AlexanderAndersson <aaandersson98@gmail.com>
2023-10-16 12:17:36 +0200

    Initial Commit

---
+++ OnDeviceUppgift3/OnDeviceUppgift3App.swift
@@ -0,0 +1,17 @@
+//
+//  OnDeviceUppgift3App.swift
+//  OnDeviceUppgift3
+//
+//  Created by Alexander Andersson on 2023-10-16.
+//
+
+import SwiftUI
+
+@main
+struct OnDeviceUppgift3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
